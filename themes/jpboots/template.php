707c642548a03483de0f28189358b8cf1655d0e7
Chris Ward <chriswhward@gmail.com>
2014-09-10 13:27:23 +0200

    Layout to make certificate

--- themes/jpboots/template.php
+++ themes/jpboots/template.php
@@ -4,3 +4,11 @@
  * @file
  * template.php
  */
+
+function jpboots_preprocess_node(&$variables) {
+    if(($variables['type'] == 'pet_character') && ($variables['view_mode'] == 'embedded_secondary_character')) {
+        $variables['second_block_header'] = '<b>OFFICIAL RESULTS:</b> Your pet shows the characteristics of these two main personality types';
+        $variables['second_block_footer'] = 'Read More at <a href="http://www.petquiz.com.au/personalities" target="_blank">www.petquiz.com.au/personalities</a>';
+        dpm($variables);
+    }
+}
