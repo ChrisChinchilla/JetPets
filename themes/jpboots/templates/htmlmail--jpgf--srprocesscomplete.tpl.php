2fa8661e844dfdf5ffcb7f49b3ad4a3d99c6513a
Chris Ward <chriswhward@gmail.com>
2014-09-11 11:38:11 +0200

    Email styling

--- themes/jpboots/templates/htmlmail--jpgf--srprocesscomplete.tpl.php
+++ themes/jpboots/templates/htmlmail--jpgf--srprocesscomplete.tpl.php
@@ -8,33 +8,42 @@ if (!strncmp($template_path, $current_path, $current_len)) {
     $template_path = substr($template_path, $current_len + 1);
 }
 $template_url = url($template_path, array('absolute' => TRUE));
+$themeURL = url(drupal_get_path('theme', $GLOBALS['theme']), array('absolute' => TRUE));
 ?>
-    <div class="htmlmail-body">
-        Congratulations <?php echo $params['name'] ?>!! Your <?php echo $params['petName'] ?> is a <?php echo $params['petType'][0] ?>.<br>
-        <br>
-        <?php
-        $config = array(
-            "style_name" => "300px_shareable",
-            "path" => $params['petImage'][0],
-            "height" => NULL,
-            "width" => NULL,
-        );
-        print theme_image_style($config);
-        ?>
-        <br>
-        <br>
-        <ul>
-            <li><a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $params['shareURL'] . '/small' ?>">Facebook</a></li>
-            <li><a href="http://twitter.com/intent/tweet?url=<?php echo $params['shareURL'] . '/small' ?>&text=Find out what personality my <?php echo $params['petName'] ?> was!?&via=jetpets">Twitter</a></li>
-            <li><a href="https://plus.google.com/share?url=<?php echo $params['shareURL'] . '/small' ?>">G+</a></li>
-            <li>Pin</li>
-            <li><a href="mailto:subject=What should this say&body=What should this say <?php echo $params['shareURL'] . '/small' ?>">Email</a>
-            </li>
-        </ul>
-        <br>
-        You can download the certificate from this <a href="<?php echo $params['url']?>">link</a> to print out and frame.<br>
-        <br>
-        Find out how <?php echo $params['petName'] ?> compares to other pets <a href="http://petquiz.com.au/personalities" target=""_blank">here</a>.<br>
-        <br>
-        <a href="http://Petquiz.com.au">Petquiz.com.au</a> is proudly supported by Jetpets.
-    </div>
+
+<div class="htmlmail-body" style="max-width:550px;">
+    Congratulations <?php echo $params['name'] ?>!! Your <?php echo $params['petName'] ?> is
+    a <?php echo $params['petType'][0] ?>.<br>
+    <br>
+    <?php
+    $config = array(
+        "style_name" => "300px_shareable",
+        "path" => $params['petImage'][0],
+        "height" => NULL,
+        "width" => NULL,
+        "attributes" => array(
+            "style" => "float: left; margin-right:10px;",
+        )
+    );
+    print theme_image_style($config);
+    ?>
+    <br>
+
+    <p>Share your result</p>
+    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $params['shareURL'] . '/small' ?>"><img
+            src="<?php echo $themeURL; ?>/images/fb_email_logo.png"></a>
+    <a href="http://twitter.com/intent/tweet?url=<?php echo $params['shareURL'] . '/small' ?>&text=Find out what personality my <?php echo $params['petName'] ?> was!?&via=jetpets"><img
+            src="<?php echo $themeURL; ?>/images/tw_email_logo.png"></a>
+    <a href="https://plus.google.com/share?url=<?php echo $params['shareURL'] . '/small' ?>"><img
+            src="<?php echo $themeURL; ?>/images/gp_email_logo.png"></a>
+    <a href="mailto:subject=What should this say&body=What should this say <?php echo $params['shareURL'] . '/small' ?>"><img
+            src="<?php echo $themeURL; ?>/images/em_email_logo.png"></a>
+
+    <br>
+    You can view the your result <a href="<?php echo $params['url'] ?>">here</a> and print out and frame if you wish.<br>
+    <br>
+    Find out how <?php echo $params['petName'] ?> compares to other pets <a href="http://petquiz.com.au/personalities"
+                                                                            target="_blank">here</a>.<br>
+    <br>
+    <a href="http://Petquiz.com.au">Petquiz.com.au</a> is proudly supported by Jetpets.
+</div>
