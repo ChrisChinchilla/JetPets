459fce71b5a3e5311d5f9322d8011f94d60b023e
Chris Ward <chriswhward@gmail.com>
2014-10-20 21:02:21 +0100

    Showing cert image edit on edit form

--- modules/custom/jpgf/jpgf.js
+++ modules/custom/jpgf/jpgf.js
@@ -1,12 +1,12 @@
 (function ($) {
     Drupal.behaviors.jpgf = {
         attach: function(context, settings) {
-            if ($('.field-name-field-pet-image').length > 0) {
-                $('.field-name-field-pet-cert-image').remove();
-                $('.field-name-field-primary-pet-character').addClass( "withImage" );
+            if ($('.node-type-survey-response .field-name-field-pet-image').length > 0) {
+                $('.node-type-survey-response .field-name-field-pet-cert-image').remove();
+                $('.node-type-survey-response .field-name-field-primary-pet-character').addClass( "withImage" );
 
             } else {
-                $('.field-name-field-primary-pet-character .node-pet-character h2').remove();
+                $('.node-type-survey-response .field-name-field-primary-pet-character .node-pet-character h2').remove();
             }
         }
     };
