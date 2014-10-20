28fa5d7b07329a0cd242e8795ed2e8b70c41e9ea
Chris Ward <chriswhward@gmail.com>
2014-10-20 21:06:27 +0100

    Undo

--- modules/custom/jpgf/jpgf.js
+++ modules/custom/jpgf/jpgf.js
@@ -1,12 +1,12 @@
 (function ($) {
     Drupal.behaviors.jpgf = {
         attach: function(context, settings) {
-            if ($('.node-type-survey-response .field-name-field-pet-image').length > 0) {
-                $('.node-type-survey-response .field-name-field-pet-cert-image').remove();
-                $('.node-type-survey-response .field-name-field-primary-pet-character').addClass( "withImage" );
+            if ($('.field-name-field-pet-image').length > 0) {
+                $('.field-name-field-pet-cert-image').remove();
+                $('.field-name-field-primary-pet-character').addClass( "withImage" );
 
             } else {
-                $('.node-type-survey-response .field-name-field-primary-pet-character .node-pet-character h2').remove();
+                $('.field-name-field-primary-pet-character .node-pet-character h2').remove();
             }
         }
     };
