1a039a522a0e03245fbbb967ef1d72a44181c5e8
Chris Ward <chriswhward@gmail.com>
2014-10-30 16:48:05 +0100

    Added feature to hide save button on image add until image added

--- modules/custom/jpgf/jpgf.js
+++ modules/custom/jpgf/jpgf.js
@@ -1,12 +1,15 @@
 (function ($) {
     Drupal.behaviors.jpgf = {
-        attach: function(context, settings) {
+        attach: function (context, settings) {
             if ($('.field-name-field-pet-image').length > 0) {
                 $('.field-name-field-pet-cert-image').remove();
-                $('.field-name-field-primary-pet-character').addClass( "withImage" );
+                $('.field-name-field-primary-pet-character').addClass("withImage");
 
             } else {
                 $('.field-name-field-primary-pet-character .node-pet-character h2').remove();
+            }
+            if ($('.imagefield-crop-preview').length > 0) {
+                $('#edit-submit').show();
             }
         }
     };
