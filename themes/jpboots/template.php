52742bb6d4ef6a3d1cc9eebe1833311cfe28b192
Chris Ward <chriswhward@gmail.com>
2014-11-08 13:39:26 +0100

    Revert social shares to SR nodes

--- themes/jpboots/template.php
+++ themes/jpboots/template.php
@@ -8,9 +8,5 @@ function jpboots_preprocess_node(&$variables) {
 
     if(($variables['type'] == 'pet') && ($variables['view_mode'] == 'embedded_pet')) {
         $_SESSION['petID'] = $variables['nid'];
-        //dpm($variables);
-//        $total = $variables['field_primary_score'][0]['value'] + $variables['field_secondary_score'][0]['value'];
-//        $variables['primPerc'] = ceil(($variables['field_primary_score'][0]['value'] / $total) * 100) . '% ' . $variables['field_primary_pet_character'][0]['entity']->title  ;
-//        $variables['secPerc'] = floor(($variables['field_secondary_score'][0]['value'] / $total) * 100) . '% ' . $variables['field_secondary_pet_character'][0]['entity']->title ;
     }
 }
